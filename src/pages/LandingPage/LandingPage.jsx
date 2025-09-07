75d6fc8e29dfc6363dc7cd1da5a427f02c43d306
Yash Gupta <yashgupta5094@gmail.com>
2025-09-07 22:56:07 +0530

    Designed Landing Page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
 import Hero from '../../components/Hero/Hero';
 import Features from '../../components/Features/Features';
 import Footer from '../../components/Footer/Footer';
@@ -8,7 +7,6 @@ import './LandingPage.css'; // optional for page-specific styles
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <Navbar />
       <Hero />
       <Features />
       <Footer />
