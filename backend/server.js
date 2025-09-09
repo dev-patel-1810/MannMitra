e938d022094d33f8234035549d33adf8ad41011c
Dev Patel <devp.delhi95@gmail.com>
2025-09-09 19:40:06 +0530

    MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+dotenv.config({ quiet: true });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,3 +19,8 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MannMitra')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('MongoDB Connection Error:', err));
