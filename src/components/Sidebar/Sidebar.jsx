2c88185cb1543d9d843a7d1f096bbb13c382c46d
Dhruv Goel <dhruvgoel654@gmail.com>
2025-09-16 22:01:37 +0530

    dashboard updation

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,12 +5,12 @@ const Sidebar = ({ isOpen }) => {
   return (
     <aside className={`sidebar ${isOpen ? "open" : "closed"}`}>
       <ul>
-        <li>Dashboard</li>
-        <li>Schedule</li>
-        <li>Peer Group</li>
-        <li>Analytics</li>
-        <li>Resource Hub</li>
-        <li>Logout</li>
+        <li className="sidebar-item">Dashboard</li>
+        <li className="sidebar-item">Schedule</li>
+        <li className="sidebar-item">Peer Group</li>
+        <li className="sidebar-item">Analytics</li>
+        <li className="sidebar-item">Resource Hub</li>
+        <li className="sidebar-item">Logout</li>
       </ul>
     </aside>
   );
