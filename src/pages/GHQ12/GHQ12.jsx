7a9207deb94e96bf15fa7602fcc783385c4156f4
Yash Gupta <yashgupta5094@gmail.com>
2025-10-04 15:18:05 +0530

    Logo thing done

--- src/pages/GHQ12/GHQ12.jsx
+++ src/pages/GHQ12/GHQ12.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/Logo.png';
 import { useNavigate } from "react-router-dom";
 import './GHQ12.css';
 
